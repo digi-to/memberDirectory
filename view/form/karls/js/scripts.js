75ae14740a386bc43d0225ad34363b7009b3e48e
warriorgiggles <vineetkrvll@gmail.com>
2016-11-27 22:34:02 -0500

    added ids on index and script file to post object

---
+++ view/form/karls/js/scripts.js
@@ -0,0 +1,26 @@
+function go() {
+  var fname =  document.getElementById("first_name").value;
+  var lname =  document.getElementById("last_name").value;
+  var email =  document.getElementById("email").value;
+  var desc =  document.getElementById("description").value;
+  var p1 =  document.getElementById("project1").value;
+  var p2 =  document.getElementById("project2").value;
+  var p3 =  document.getElementById("project3").value;
+  var projects = [p1, p2, p3];
+
+var post_obj = {
+
+  "fname" :  fname,
+  "lname" :  lname,
+  "email" :  email,
+  "desc" :  desc,
+  "projects" : projects
+  }
+console.log(post_obj);
+var request = new XMLHttpRequest();
+request.open("post", "/newuser", true);
+request.send(post_obj);
+
+
+
+}
