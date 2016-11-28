9ac999aa25fc43a0b1269e9b71f644750656f7b8
Minh <hlminh2000@gmail.com>
2016-11-27 20:01:39 -0500

    creates default get

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 var	express 	= require('express'),
 		fs			= require('fs'),
 		app 		= express(),
-		PORT		= process.env.PORT || 80;
+		PORT		= process.env.PORT || 4000;
 
 
 app.use(express.static('view/build'));
+
+app.get('/', (req, res) => {
+	res.send(fs.readFileSync('view/form/index.html', 'utf8'));
+	res.en();
+})
 
 app.get('/form', (req, res) => {
 	res.send(fs.readFileSync('view/form/index.html', 'utf8'));
