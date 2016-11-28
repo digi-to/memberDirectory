a1ec8c2d06cf6114b69541716dd8b1f5aab3d213
Minh <hlminh2000@gmail.com>
2016-11-27 20:07:48 -0500

    stuff

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 var	express 	= require('express'),
 		fs			= require('fs'),
 		app 		= express(),
-		PORT		= process.env.PORT || 4000;
+		PORT		= process.env.PORT || 80;
 
 
-app.use(express.static('view/build'));
+app.use('/',express.static('view/build'));
 
-app.get('/', (req, res) => {
-	res.send(fs.readFileSync('view/form/index.html', 'utf8'));
-	res.end();
-})
+// app.get('/', (req, res) => {
+// 	res.send(fs.readFileSync('view/form/index.html', 'utf8'));
+// 	res.end();
+// })
 
 app.get('/form', (req, res) => {
 	res.send(fs.readFileSync('view/form/index.html', 'utf8'));
