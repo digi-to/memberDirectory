a15d54b03526b2688ac94886b81d20142121bf7b
Minh <hlminh2000@gmail.com>
2016-11-27 19:57:57 -0500

    changes port to 80

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var	express 	= require('express'),
 		fs			= require('fs'),
 		app 		= express(),
-		PORT		= process.env.PORT || 4000;
+		PORT		= process.env.PORT || 80;
 
 
 app.use(express.static('view/build'));
