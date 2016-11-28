b9dc8dfbebe68aff127a636833e76ba6a04aa75e
Minh <hlminh2000@gmail.com>
2016-11-27 21:50:10 -0500

    sets up some api end points

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 var	express 	= require('express'),
 		fs			= require('fs'),
 		app 		= express(),
-		PORT		= process.env.PORT || 4000;
+		mysql		= require('mysql'),
+		queries	= require('./sqlQueries.js'),
+		PORT		= process.env.PORT || 4000,
+		connection = mysql.createConnection({
+			host     : 'localhost',
+			user     : 'root',
+			password : 'root',
+			database : 'HACKATHON'
+		});
 
+execsql.config(dbConfig)
+	.exec(sql).end();
+
+api.use(bodyParser.urlencoded({ extended:false }));
+api.use(bodyParser.json());
 
 app.use('/',express.static('view/directory'));
 
@@ -14,9 +27,15 @@ app.use('/form', express.static('view/form/karls'));
 // 	res.end();
 // });
 
-// app.post('/newUser', (req, res) => {
-// 	var userData = req.data;
-// });
+app.post('/newUser', (req, res) => {
+	console.log(req.body);
+	var userData = req.body;
+	var queryStr = queries.newUser(userData.fname, userData.lname, userData.email, userData.desc, userData.projects);
+	connection.query(queryStr, (err, rows, fields) => {
+		if (err) throw err;
+	});
+	connection.end();
+});
 
 app.listen(PORT, ()=>{
 	console.log("Listening on port: " + PORT);
---
+++ server/sqlQueries.js
@@ -0,0 +1,9 @@
+var queries = {
+	newUser: function(fname, lname, email, desc, projects){
+		return "INSERT INTO members (first_name, last_name, id, email, description, projects) VALUES ("
+			+ [fname, lname, "NULL",  email, desc, projects.join(', ')].join(', ')
+			+ ");";
+	}
+}
+
+exports.queries = queries;
