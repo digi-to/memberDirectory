32002838d8cd744b0b3d0c9c37f649de02fbb4b4
Minh <hlminh2000@gmail.com>
2016-11-27 19:43:18 -0500

    stuff

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-var express = require('express');
+var	express 	= require('express'),
+		fs			= require('fs'),
+		app 		= express(),
+		PORT		= 4000;
+
+
+app.use(express.static('view/build'));
+
+app.get('/form', (req, res) => {
+	res.send(fs.readFileSync('view/form/index.html', 'utf8'));
+	res.en();
+});
+
+// app.post('/newUser', (req, res) => {
+// 	var userData = req.data;
+// });
+
+app.listen(PORT, ()=>{
+	console.log("Listening on port: " + PORT);
+});
